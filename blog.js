905bce81efe28bb1b861a12c96ad031f4e93b178
tangyu <951325316@qq.com>
2017-11-01 22:54:39 +0800

    我的静面简历页面

---
+++ blog.js
@@ -0,0 +1,11 @@
+showreel1.onclick = function () {
+    showreel.className = "progressBar state-1"
+};
+
+showreel2.onclick = function () {
+    showreel.className = "progressBar state-2"
+};
+
+showreel3.onclick = function () {
+    showreel.className = "progressBar state-3"
+};
